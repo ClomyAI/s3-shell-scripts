3b2676f3dd8cdb6c71aa1d22bcf714c6cb78efcf
CloudemyTV <65519054+CloudemyTV@users.noreply.github.com>
2021-01-18 14:25:46 +0800

    Update s3_mpupload.sh
    Adding retry to parts upload in case of network interruption

--- s3_mpupload.sh
+++ s3_mpupload.sh
@@ -36,17 +36,25 @@ DIR="PART_$(date +%s)"
 mkdir $DIR
 split -b $SIZE $KEY $DIR/ # create part files in temp directory
 i=0
+# PART_JSON='{ "Parts": []}'
 
 for file in $(ls $DIR) # list all part files
 do
-  i=$((i+1))
-  # Use MD5 checksum to validate the integrity of the upload for each part
-  MD5=$(openssl md5 -binary $DIR/$file | base64)
+   i=$((i+1))
+   # Use MD5 checksum to validate the integrity of the upload for each part
+   MD5=$(openssl md5 -binary $DIR/$file | base64)
   
-  # AWS Cli - Upload parts to S3, keep the ETag
-  ETAG=$(aws --profile $PROFILE s3api upload-part --bucket $BUCKET --key $KEY --part-number $i --body $DIR/$file --upload-id $UPLOAD_ID --content-md5 $MD5 | jq -r '.ETag')
-  echo "Part number $i is complete, ETag is $ETAG"
+   ETAG=
+   # Retry upload parts if network is interrupted
+   until [ ! -z $ETAG ]
+   do
+      # AWS Cli - Upload parts to S3, keep the ETag
+      ETAG=$(aws --profile $PROFILE s3api upload-part --bucket $BUCKET --key $KEY --part-number $i --body $DIR/$file --upload-id $UPLOAD_ID --content-md5 $MD5 | jq -r '.ETag')
+      echo "Part number $i is complete, ETag is $ETAG"
+   done
   
+   # Preparing the JSON required for completing uploads
+   # PART_JSON=$(echo $PART_JSON | jq --arg i $i --arg ETAG $ETAG '.Parts += [{"PartNumber": $i|tonumber, "ETag": $ETAG }]') # use tonumber to parse string to integer
 done
 
 # List parts that have been uploaded and preparing the JSON required for completing uploads
@@ -59,3 +67,4 @@ echo $PART_JSON > $DIR/part.json # create JSON file of uploaded parts
 aws s3api --profile $PROFILE complete-multipart-upload --multipart-upload file://$DIR/part.json --bucket $BUCKET --key $KEY --upload-id $UPLOAD_ID
 
 # rm -r $DIR
+
